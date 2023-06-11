cf00b6bb575a633e79c07e485585b641e88e9ccb
simonjiho <simonjiho@gmail.com>
2023-06-11 18:05:42 +0900

    Initial Commit

---
+++ recoraddiction/recoraddictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  recoraddictionApp.swift
+//  recoraddiction
+//
+//  Created by 김지호 on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct recoraddictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
